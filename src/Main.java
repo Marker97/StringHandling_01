52bbdb18c709a9512aaab82bd7654e5a70587a14
Marker97 <chereche10@gmail.com>
2022-11-16 17:42:33 +0100

    StringHandling_01

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String string1 = "Hello";
+        String string2 = "How are you?";
+        char [] charArray = {string1.charAt(0), string1.charAt(1),string2.charAt(10), string2.charAt(11)};
+
+        System.out.println(charArray);
+    }
+}
